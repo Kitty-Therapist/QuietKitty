8bd1ff76014e30608e0acbfbf97549b7e72bbf33
Kitty-Therapist <31807465+Kitty-Therapist@users.noreply.github.com>
2018-06-17 11:30:28 -0400

    Updated.
    Made some changes.

--- QuietKitty/cogs/announce.py
+++ QuietKitty/cogs/announce.py
@@ -23,6 +23,13 @@ class AnnounceCog:
             await ctx.trigger_typing()
             t2 = time.perf_counter()
             await ctx.send(f":hourglass: Gateway Ping is {round((t2 - t1) * 1000)}ms :hourglass:")
+            
+    @commands.command(hidden=True)
+    async def restart(self, ctx):
+        """Restarts the bot"""
+        await ctx.send("Restarting...")
+        await self.bot.logout()
+        await self.bot.close()
 
     @commands.command()
     @commands.bot_has_permissions(manage_roles=True)
@@ -59,7 +66,7 @@ class AnnounceCog:
         if(felics != ""):
             try:
                 await role.edit(mentionable=True)
-                await channel.send(f"⸤ <@&353539085723369502> ⸣ {felics}\nGo here at https://www.twitch.tv/felixinfintum !\n\nIf you would like to un-/assign the role. Head on over to <#390296878773960705> and do ``_selfassign Felics' Stream``")
+                await channel.send(f"⸤ <@&353539127561551872> ⸣ {felics}\nGo here at https://www.twitch.tv/felixinfintum !\n\nIf you would like to un-/assign the role. Head on over to <#390296878773960705> and do ``_selfassign Felics' Stream``")
                 await role.edit(mentionable=False)      
             except discord.Forbidden:
                 await ctx.send("I wasn't able to send a message in the streaming channel. Please check that I am able to talk.")
@@ -147,6 +154,46 @@ class AnnounceCog:
                 await role.edit(mentionable=False)      
             except discord.Forbidden:
                 await ctx.send("I wasn't able to send a message in the streaming channel. Please check that I am able to talk.")
+        else: 
+            await ctx.send("What are you doing? :(")
+    
+    @commands.command()
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.guild_only()
+    async def shoutouts(self, ctx: commands.Context, *,shoutouts = ""):
+        channel= ctx.bot.get_channel(390300266244866049)
+        role_id= 439468306148360202
+        role= discord.utils.find(lambda r: r.id == role_id, ctx.guild.roles)
+        if role is None:
+            await ctx.send("Did you delete that role, <@186002153066725378>?")
+            return 
+            
+        if(shoutouts != ""):
+            try:
+                await role.edit(mentionable=True)
+                await channel.send(f"⸤ <@&439468306148360202> ⸣ {shoutouts}\n\nIf you would like to un-/assign the role. Head on over to <#390296878773960705> and do ``_selfassign Shoutouts``!")
+            except discord.Forbidden:
+                await ctx.send("I wasn't able to send a message in the streaming channel. Please check that I am able to talk.")
+        else: 
+            await ctx.send("What are you doing? :(")
+    
+    @commands.command()
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.guild_only()
+    async def family(self, ctx: commands.Context, *,family = ""):
+        channel= ctx.bot.get_channel(456233324269273129)
+        role_id= 390303538288394240
+        role= discord.utils.find(lambda r: r.id == role_id, ctx.guild.roles)
+        if role is None:
+            await ctx.send("Did you delete that role, <@186002153066725378>?")
+            return 
+            
+        if(family != ""):
+            try:
+                await role.edit(mentionable=True)
+                await channel.send(f"⸤ <@&390303538288394240> ⸣ {family}")
+            except discord.Forbidden:
+                await ctx.send("I wasn't able to send a message in the family announcement channel. Please check that I am able to talk.")
         else: 
             await ctx.send("What are you doing? :(")
 
